2943a0b6c9f95fca36bb8fdec39d99208cab9502
Sunny Pawar <142238741+theXunnY@users.noreply.github.com>
2025-07-01 17:08:29 +0530

    Update index.jsx
    duplicate image issue fixed

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -4,7 +4,7 @@ import project3 from "../assets/project3.png";
 import project4 from "../assets/project4.png";
 import project5 from "../assets/project5.png";
 import project6 from "../assets/project6.png";
-import project6 from "../assets/project7.png";
+import project7 from "../assets/project7.png";
 
 import {
   RiHtml5Line,
